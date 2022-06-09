02350f81ba26928cec52af9bd3460600d8fc5b2f
mborreros <mlbsindia@gmail.com>
2022-06-09 14:04:38 -0400

    commit tree f4a9ed957548ca487d0764254cf28100750dc31b

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,41 @@
 class ItemsController < ApplicationController
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find_by(id: params[:user_id])
+      if user
+        items = user.items
+      else 
+        render json: {error: "user not found"}, status: :not_found
+        return
+      end
+    else
+      items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  def show
+    user = User.find_by(id: params[:user_id])
+    item = user.items.find_by(id: params[:id])
+    if item
+      render json: item, include: :user
+    else 
+      render json: {error: "no item was found"}, status: :not_found
+    end
+  end
+
+  def create
+    user = User.find_by(id: params[:user_id])
+    new_item = user.items.create(item_params)
+    render json: new_item, inlcude: :user, status: :created
+    # byebug
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price)
   end
 
 end
